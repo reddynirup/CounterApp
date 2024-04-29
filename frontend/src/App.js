1ad56cbe7df923053f34725701049d0d8ec15fcf
nirupreddy <nirupreddy2004@gmail.com>
2024-04-30 05:27:20 +0530

    CHALLENGE completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, useReducer, useCallback } from 'react';
+import React, {useEffect, useContext, useReducer, useCallback } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
