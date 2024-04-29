69bfdd13c00fb801bfecb6d37f86e9ad1f897a74
nirupreddy <nirupreddy2004@gmail.com>
2024-04-30 05:01:48 +0530

    CHALLENGE completed

--- backend/App.js
+++ backend/App.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv=require("dotenv");
+const path=require("path");
 dotenv.config();
 const app = express();
 
